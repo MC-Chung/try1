090f0c77b0d7b84080810bd1a0b2a91a3c4b4adf
MC-Chung <hermanchungmc@gmail.com>
2018-08-29 22:58:55 +0800

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+  get 'pages/pricing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
